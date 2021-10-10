bf4b9e5da8ac01ed3b287eb532f774f1f63401f4
Sebastian <sebastian.akerhielm.8786@student.uu.se>
2021-10-10 04:42:33 +0200

    Kod och lite grejor

---
+++ InfiTac.py
@@ -0,0 +1,72 @@
+# Säg till om du har något du tycker borde ändras
+
+# Måste göras, kan finnas mer:
+#   Turnstate
+#   Main
+#   User Input
+#   AI
+
+
+def createBoard(size: int): # KLAART
+    board = [["_"]*size]*size
+    return board
+
+def viewBoard(board: list): # KLAART
+    for i in range(len(board)):
+        boardRow = ""
+        for j in range(len(board[i])):
+            boardRow += board[i][j]
+        print(boardRow)
+    pass
+
+# Typ om man vill restarta eller nåt
+def menu(title: str, prompt: str, options: dict): # KLAART 
+    print(title)
+
+    for k, v in options.items():
+        print(f"{k}) {v}")
+
+    print()
+    userSelection = None
+    while True:
+        userSelection = input(prompt)
+        if userSelection in options:
+            break
+    print()
+    return userSelection
+
+def boardState(board: list, newInput: list , playerTile: str): # NOT DONE
+    # newInput är på formen [x,y] för enkelhetens skull
+
+    #typ sätt in ny tile som spelaren sa och sen kolla i alla riktningar för möjliga matchningar med regex eller nåt och rowCount()
+    board[newInput[0]][newInput[1]] = playerTile
+
+    pass
+
+
+
+#region Statiska variabler
+
+# hasattr() kollar om funktionen är initierad eller inte när den används. Om inte så sätts värdet till ett standardvärde.
+
+def player1Tile(tile = None):   # KLAART
+    if tile != None:
+        player1Tile.savedTile = tile
+    elif not hasattr(player1Tile, "savedTile"):
+        player1Tile.savedTile = "x"
+    return player1Tile.savedTile
+
+def player2Tile(tile = None):   # KLAART
+    if tile != None: 
+        player2Tile.savedTile = tile
+    elif not hasattr(player2Tile, "savedTile"):
+        player2Tile.savedTile = "o"
+    return player2Tile.savedTile
+
+def rowCount(num = None):   # KLAART
+    if num != None:
+        rowCount.savedNum = num
+    elif not hasattr(rowCount, "savedNum"):
+        rowCount.savedNum = 3
+    return rowCount.savedNum
+#endregion
