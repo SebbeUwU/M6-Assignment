1b5d46dd04695f42538255aedbe632d7e503fd63
Sebastian <sebastian.akerhielm.8786@student.uu.se>
2021-10-23 01:35:28 +0200

    fixed it

--- InfiTac.py
+++ InfiTac.py
@@ -1,4 +1,6 @@
 import re
+import os
+
 # Säg till om du har något du tycker borde ändras
 
 
@@ -60,41 +62,44 @@ def emptyBoardTile(tile = None):
 
 def saveBoard(board: list, currentPlayer, saveName: str):
     open(saveName+".txt", "w").close()
-    save = open(saveName+".txt", "w")
-    save.write(f"BoardSize: {len(board)}")
-    for i in range(len(board)):
-        boardRow = "".join([_ for _ in board[i]])
-        save.write(boardRow)
+    with open(saveName+".txt", "w") as save:
+        save.write(f"BoardSize: {len(board)}\n")
+        for i in range(len(board)):
+            boardRow = " ".join([_ for _ in board[i]])
+            save.write(boardRow + "\n")
     
-
-    save.write(f"CurrentPlayer: {currentPlayer}")
-    save.write(f"Player2: {player2Tile() if currentPlayer == player1Tile() else player1Tile()}")
-    save.write(f"EmptyTile: {emptyBoardTile()}")
-    save.close()
+        save.write(f"CurrentPlayer: {currentPlayer}\n")
+        save.write(f"Player2: {player2Tile() if currentPlayer == player1Tile() else player1Tile()}\n")
+        save.write(f"EmptyTile: {emptyBoardTile()}\n")
     pass
 
 def loadBoard(saveName: str):
-    save = open(saveName+".txt", "r")
-    line = save.readline()
-    boardSize = int(line.split("BoardSize: ")[1])
-
     board = []
-    for i in range(boardSize):
-        board[i] = save.readline().split()
+    with open(saveName+".txt", "r") as save:
+        boardSize = int(save.readline().split("BoardSize: ")[1])
 
-    player1Tile(line.split("CurrentPlayer: ")[1])
-    player2Tile(line.split("Player2: ")[1])
-    emptyBoardTile(line.split("EmptyTile: ")[1])
+        for i in range(boardSize):
+            boardRow = save.readline()
+            boardRow = boardRow[0:-1]
+            boardRow = boardRow.split(" ")
+            board.append(boardRow)
+
+        player1Tile(save.readline().split("CurrentPlayer: ")[1][0:-1])
+        player2Tile(save.readline().split("Player2: ")[1][0:-1])
+        emptyBoardTile(save.readline().split("EmptyTile: ")[1][0:-1])
+
     return board
     
 
 def createBoard(size: int): # KLAAR
     board = []
     for i in range(size):
-        boardRow = [emptyBoardTile()]*size
-        board.append(boardRow)
-
-
+        columns = []
+        for j in range(size):
+            columns.append(emptyBoardTile())
+        board.append(columns)
+    
+    print(board)
    # board = [[emptyBoardTile()]*size]*size
     #board = [board[:] for i in range(size)]
     return board
@@ -131,25 +136,10 @@ def menu(title: str, prompt: str, options: dict): # KLAART
 
     return userSelection
 
-def boardState(board: list, newInput: list , playerTile: str): # Klar
-    x = newInput[0]
-    y = newInput[1]
-    board[y][x] = playerTile
-    viewBoard(board)
-    gameOver = winCon(board, newInput, playerTile)
-
-    if gameOver == True:
-        print(f"Player {playerTile} won!")
-    elif gameOver == None:
-        print("It's a tie!")
-        gameOver = True
-    
-    return not gameOver
-
 def winCon(board: list, newInput: list, playerTile: str): # KLAAR den gör nåt
     pattern = re.compile(f"{playerTile*winLength(board)}")
     boardRow = "".join([_ for _ in board[newInput[1]]])
-    boardColumn = "".join(board[:][newInput[0]])
+    boardColumn = "".join([row[newInput[0]] for row in board])
     
     if pattern.search(boardRow) or pattern.search(boardColumn):
         return True
@@ -198,21 +188,35 @@ def winCon(board: list, newInput: list, playerTile: str): # KLAAR den gör nåt
     return False
 
 
-def play(board = createBoard(3)):
-    currentPlayer = player1Tile()
-    gameOver = True
-    while gameOver:
+def play(startBoard, boardName="LastGame"):
+    currentPlayer = player2Tile()
+    board = startBoard
+    gameOver = False
+    while not gameOver:
+        if currentPlayer == player1Tile():
+            currentPlayer = player2Tile()
+        else:
+            currentPlayer = player1Tile()
+
+        print(f"Player {currentPlayer}'s turn\n")
+        viewBoard(board)
+        print()
+
+        saveBoard(board, currentPlayer, boardName)
+
         while True:
             userInput = input("x, y: ")
+            
             try:
                 emptyTile = True
                 userInput = userInput.split(", ")
                 userInputList = [int(userInput[0]), int(userInput[1])]
-                if board[userInputList[0]][userInputList[1]] != "_":
+                x = userInputList[0]
+                y = userInputList[1]
+
+                if board[y][x] != "_":
                     raise TypeError
 
-                print(userInputList)
-                boardTest = board[userInputList[0]][userInputList[1]]
                 break
                 
             except ValueError:
@@ -225,19 +229,26 @@ def play(board = createBoard(3)):
             except IndexError:
                 print("Out of range")
                 continue
-                
-        gameOver = boardState(board, userInputList, currentPlayer)
         
-        if currentPlayer == player1Tile():
-            currentPlayer = player2Tile()
-        else:
-            currentPlayer = player1Tile()
+        print()
+        board[y][x] = currentPlayer
+        gameOver = winCon(board, userInputList, currentPlayer) 
+
+    if gameOver == True:
+        print(f"Player {currentPlayer} won!\n")
+    elif gameOver == None:
+        print("It's a tie!\n")
+        gameOver = True
+
+    viewBoard(board)
+    print()
+
     pass
         
 def main():
     menuItems = {"p":"Play TicTacToe", "c":"Custom Game", "l":"Load Game", "q":"Quit"}
     while True:
-        option = menu("Welcome to InfiniTac, what would you like to do", "Option: ", menuItems)
+        option = menu("Welcome to InfiniTac, what would you like to do\n", "Option: ", menuItems)
         if option == "p":
             player1Tile("x")
             player2Tile("o")
@@ -245,15 +256,26 @@ def main():
             winLength(board, 3)
             play(board)
         elif option == "c":
-            player1Tile(int(input("Choose a tile for player 1: ")))
-            player2Tile(int(input("Choose a tile for player 2: ")))
+            player1Tile(input("Choose a tile for player 1: "))
+            player2Tile(input("Choose a tile for player 2: "))
             board = createBoard(int(input("Choose a boardsize: ")))
-            winLength(int(input("Choose amount of tiles in a row to win: ")))
-            play(board)
+            winLength(board, int(input("Choose amount of tiles in a row to win: ")))
+            saveName = input("Choose name of save: ")
+            play(board, saveName)
             
         elif option == "l":
-            
-            play(board)
+            print("Existing saves or input b to go back:")
+            files = [f for f in os.listdir('.') if os.path.isfile(f)]
+
+            [print(e[0:-4]) for e in files if ".txt" in e]
+        
+            saveName = input("\nPick save to continue: ")
+
+            if saveName == "b":
+                continue
+            board = loadBoard(saveName)
+            play(board, saveName)
+
         elif option == "q":
             print("Thank you for playing")
             break
